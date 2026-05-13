96c980db8ec05981e7b56f5eb52e4961f3ed3102
ma7moud7addad <ma7moud7addad@gmail.com>
2026-05-13 19:23:57 +0300

    Prevent transparent screen from hiding keyboard
    Added a flag to prevent the transparent screen from hiding the original keyboard or taking focus away from it.

--- app/src/main/java/helium314/keyboard/latin/BiometricActivity.java
+++ app/src/main/java/helium314/keyboard/latin/BiometricActivity.java
@@ -2,6 +2,7 @@ package helium314.keyboard.latin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.biometric.BiometricPrompt;
@@ -11,6 +12,8 @@ import java.util.concurrent.Executor;
 public class BiometricActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // السر هنا: منع الشاشة الشفافة من إخفاء الكيبورد الأصلي أو سحب التركيز منه
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
         super.onCreate(savedInstanceState);
         
         Executor executor = ContextCompat.getMainExecutor(this);
@@ -18,14 +21,13 @@ public class BiometricActivity extends FragmentActivity {
             @Override
             public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
-                restoreKeyboardOnly(); // نرجع الكيبورد لو قفلنا البصمة
+                restoreKeyboardOnly(); 
                 finish(); 
             }
 
             @Override
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-                // البصمة نجحت! نبعت إشارة سرية للكيبورد عشان يظهر ويفتح الحافظة
                 Intent intent = new Intent(BiometricActivity.this, LatinIME.class);
                 intent.setAction("com.mahmoud.OPEN_CLIPBOARD_NATIVE");
                 startService(intent);
@@ -35,7 +37,6 @@ public class BiometricActivity extends FragmentActivity {
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
-                // لو البصمة غلط، هيفضل فاتح يخليه يجرب تاني
             }
         });
 
@@ -48,7 +49,6 @@ public class BiometricActivity extends FragmentActivity {
         biometricPrompt.authenticate(promptInfo);
     }
 
-    // دالة صغيرة لإرجاع الكيبورد بدون فتح الحافظة
     private void restoreKeyboardOnly() {
         Intent intent = new Intent(this, LatinIME.class);
         intent.setAction("com.mahmoud.RESTORE_KEYBOARD");
