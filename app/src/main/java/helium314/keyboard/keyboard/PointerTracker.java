42038ec54b17c2ada314c88c8fab280ec24d4624
ma7moud7addad <ma7moud7addad@gmail.com>
2026-05-21 22:10:55 +0300

    Reduce sensitivity for delete action from 7 to 5
    Decrease sensitivity for delete action to improve performance.

--- app/src/main/java/helium314/keyboard/keyboard/PointerTracker.java
+++ app/src/main/java/helium314/keyboard/keyboard/PointerTracker.java
@@ -969,7 +969,7 @@ public final class PointerTracker implements PointerTrackerQueue.Element,
             }
         } else if (code == KeyCode.DELETE) {
             // --- بداية تعديل MacBoard (زيادة سرعة وحساسية السحب للحذف) ---
-            int sensitivity = 7; 
+            int sensitivity = 5; 
             int deleteStep = Math.max(1, sPointerStep / sensitivity);
 
             // Delete slider
