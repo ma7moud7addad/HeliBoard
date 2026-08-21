a039f479e2062e6d0a3c939228f205f1400282d7
ma7moud7addad <ma7moud7addad@gmail.com>
2026-08-21 08:15:58 +0300

    Update ClipboardHistoryEntry.kt

--- app/src/main/java/helium314/keyboard/latin/ClipboardHistoryEntry.kt
+++ app/src/main/java/helium314/keyboard/latin/ClipboardHistoryEntry.kt
@@ -1,16 +1,12 @@
 package com.macboard.keyboard.latin
 
-import android.annotation.SuppressLint
 import android.content.ClipDescription
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.os.Build
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.FileProvider
 import androidx.core.view.inputmethod.InputContentInfoCompat
-import com.macboard.keyboard.latin.common.ColorType
-import com.macboard.keyboard.latin.settings.Settings
 import java.io.File
 
 class ClipboardHistoryEntry(
@@ -56,7 +52,6 @@ class ClipboardHistoryEntry(
         return InputContentInfoCompat(uri, desc, null)
     }
 
-    @SuppressLint("SetTextI18n")
     fun setImageAndDescription(imageView: ImageView, textView: TextView) {
         if (mimeTypes == null || filename == null) return
         try {
@@ -76,19 +71,8 @@ class ClipboardHistoryEntry(
             }
         } catch (e: Exception) {
         }
-        val description = if (text.isBlank()) ""
-            else "\n" + textView.context.getString(R.string.item_description, text)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            try {
-                val info = imageView.context.contentResolver.getTypeInfo(mimeTypes[0])
-                info.icon.setTint(Settings.getValues().mColors.get(ColorType.EMOJI_CATEGORY))
-                imageView.setImageIcon(info.icon)
-                textView.text = textView.context.getString(R.string.item_type, info.label.toString()) + description
-                return
-            } catch (_: Exception) {}
-        }
-        imageView.setImageResource(R.drawable.ic_dictionary)
-        Settings.getValues().mColors.setColor(imageView, ColorType.EMOJI_CATEGORY)
-        textView.text = textView.context.getString(R.string.item_type, mimeTypes.first()) + description
+        // لو الصورة ماتحملتش، اعرض text
+        imageView.setImageDrawable(null)
+        textView.text = text.take(1000)
     }
 }
